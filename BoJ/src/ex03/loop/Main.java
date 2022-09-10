405f93abec70960f240cfcdb0ca18bbee285ffa5
nuguseyo024 <nuguseyo024@naver.com>
2022-09-10 23:58:39 +0900

    백준 8393번 :: 합 - 자바 Java

--- BoJ/src/ex03/loop/Main.java
+++ BoJ/src/ex03/loop/Main.java
@@ -6,27 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int total = sc.nextInt();
-		int cnt = sc.nextInt();
-
-		int arr[] = new int[cnt];
+		int n = sc.nextInt();
 		int sum = 0;
-		
-		for(int i = 0; i < cnt; i++) {
-			int goods = sc.nextInt();
-			int count = sc.nextInt();
-			
-			arr[i] = goods * count ;
-			sum += arr[i];
+		for (int i = 1; i < n+1 ; i ++) {
+			sum += i;
 		}
-		
-		if(sum == total) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
-		}	
-		
+		System.out.println(sum);
 		sc.close();
 	}
-
 }
