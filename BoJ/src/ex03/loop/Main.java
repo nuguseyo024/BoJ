de7b2f147344c5794931badef80db32559eb4d01
nuguseyo024 <nuguseyo024@naver.com>
2022-09-11 14:41:30 +0900

    22-09-11 	백준 11021번 :: A+B - 7 - 자바 Java

--- BoJ/src/ex03/loop/Main.java
+++ BoJ/src/ex03/loop/Main.java
@@ -2,26 +2,26 @@ package ex03.loop;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine());
+		int testcase = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st;
 		
-		for(int i = 0; i < N; i++) {
+		for(int i = 1; i <= testcase; i++) {
 			st = new StringTokenizer(br.readLine()," ");
-			bw.write((Integer.parseInt(st.nextToken()) 
-					+ Integer.parseInt(st.nextToken())) + "\n");
+			int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+			bw.write("Case #"+i+": "+sum+"\n");
 		}
+		
 		br.close();
 		bw.flush();
 		bw.close();
