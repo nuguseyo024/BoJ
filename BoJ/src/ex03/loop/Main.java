a4f5a7b2f2249bd6804bbcbee87dd767bfbe4aba
nuguseyo024 <nuguseyo024@naver.com>
2022-09-15 21:32:38 +0900

    백준 1110번 :: 더하기 사이클 - 자바 Java

--- BoJ/src/ex03/loop/Main.java
+++ BoJ/src/ex03/loop/Main.java
@@ -1,29 +1,24 @@
 package ex03.loop;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		String input = "";
-		int a = 0;
-		int b = 0;
-		
-		while((input=br.readLine())!=null) {
-			st = new StringTokenizer(input," ");
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			bw.write(a+b+"\n");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int copy = n;
+		int count = 0;
+
+		while(true) {
+			count++;
+			n = ((n%10)*10) + (((n/10) + (n%10))% 10);
+			
+			if(copy == n) {
+				break;
+			}
 		}
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(count);
+		
 	}
 }
