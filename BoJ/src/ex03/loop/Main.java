8f8ae50fba6b20f88e65403bea5e380d12bb5e72
nuguseyo024 <nuguseyo024@naver.com>
2022-09-15 19:54:13 +0900

    백준 10952번 :: A+B - 5 - 자바 Java

--- BoJ/src/ex03/loop/Main.java
+++ BoJ/src/ex03/loop/Main.java
@@ -9,22 +9,21 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		st = new StringTokenizer(br.readLine()," ");
-		int num = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		int[] arr = new int[num];
 		
-		st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0; i < num; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());	
-			if(arr[i] < x) {
-				bw.write(arr[i]+" ");
-			}
+		while(true) {
+			st = new StringTokenizer(br.readLine()," ");
 			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a!=0 && b!=0) {
+				bw.write(a+b+"\n");
+			}else if(a==0 && b==0) {
+				break;
+			}
 		}
 		bw.flush();
 		bw.close();
