5c76f66f834d31948489204863cf4f15867ba0f9
nuguseyo024 <nuguseyo024@naver.com>
2022-09-16 23:28:32 +0900

    백준 2525번 :: 오븐 시계 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -1,23 +1,39 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());		
+		int hour = Integer.parseInt(st.nextToken());
+		int min = Integer.parseInt(st.nextToken());
 		
-		bw.write("|\\_/|");
-		bw.write("\n");
-		bw.write("|q p|   /}");
-		bw.write("\n");
-		bw.write("( 0 )\"\"\"\\");
-		bw.write("\n");
-		bw.write("|\"^\"`    |");
-		bw.write("\n");
-		bw.write("||_/=\\\\__|");
+		st = new StringTokenizer(br.readLine());
+		int time = Integer.parseInt(st.nextToken());
 		
+		int mintime = min + time;
+		if(mintime >= 60) {
+			hour = hour + (mintime/60);
+
+			min = mintime - ((mintime/60)*60);
+			if(hour >= 24) {
+				hour = hour - 24;
+			}
+		}else if(mintime < 60) {
+			min = mintime;
+		}
+
+		bw.write(hour+" "+min);
+			
 		bw.flush();
 		bw.close();
+		br.close();
+		
 	}
 
 }
