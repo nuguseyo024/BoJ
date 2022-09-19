e897a690a949ff0427fe478629a7cd30e55ca656
nuguseyo024 <nuguseyo024@naver.com>
2022-09-19 20:54:58 +0900

    22-09-19 백준 2480번 :: 주사위 세 개 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -9,27 +9,36 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());		
-		int hour = Integer.parseInt(st.nextToken());
-		int min = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		st = new StringTokenizer(br.readLine());
-		int time = Integer.parseInt(st.nextToken());
-		
-		int mintime = min + time;
-		if(mintime >= 60) {
-			hour = hour + (mintime/60);
+		int dice1 = Integer.parseInt(st.nextToken());
+		int dice2 = Integer.parseInt(st.nextToken());
+		int dice3 = Integer.parseInt(st.nextToken());
 
-			min = mintime - ((mintime/60)*60);
-			if(hour >= 24) {
-				hour = hour - 24;
-			}
-		}else if(mintime < 60) {
-			min = mintime;
+		int price = 0;
+		int max = 0; 
+		
+		// 모두 다른 눈이 나오는 경우 
+		if(dice1!=dice2 && dice2!=dice3) {
+			max = dice1 > dice2 ? (dice1 > dice3? dice1 : dice3) : (dice2 > dice3 ? dice2 : dice3);
+			price = (max * 100);			
 		}
+		
+		if(dice1 == dice2 && dice2 == dice3) {
+			price = (10000+(dice2*1000));
+		} else if(dice1==dice2 || dice1 == dice3 || dice2 == dice3) {
+			if(dice1==dice2) {
+				price = 1000 + (dice1 * 100);
+			}
+			if(dice1==dice3) {
+				price = 1000 + (dice1 * 100);
+			}
+			if(dice3==dice2) {
+				price = 1000 + (dice3 * 100);
+			}
+		} 
 
-		bw.write(hour+" "+min);
-			
+		bw.write(Integer.toString(price));
 		bw.flush();
 		bw.close();
 		br.close();
