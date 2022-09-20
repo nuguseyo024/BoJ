57fed5b3377e53c6b6d66f9c562582f0da5cc356
nuguseyo024 <nuguseyo024@naver.com>
2022-09-20 18:07:06 +0900

    22-09-20 백준 10818번 :: 최소, 최대 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,34 +12,20 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		int dice1 = Integer.parseInt(st.nextToken());
-		int dice2 = Integer.parseInt(st.nextToken());
-		int dice3 = Integer.parseInt(st.nextToken());
-
-		int price = 0;
-		int max = 0; 
+		int n = Integer.parseInt(st.nextToken());
+		int max = 0;
+		int min = 0;
 		
-		// 모두 다른 눈이 나오는 경우 
-		if(dice1!=dice2 && dice2!=dice3) {
-			max = dice1 > dice2 ? (dice1 > dice3? dice1 : dice3) : (dice2 > dice3 ? dice2 : dice3);
-			price = (max * 100);			
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i < arr.length ; i++) {					
+			arr[i] = Integer.parseInt(st.nextToken());		
 		}
+		Arrays.sort(arr);
+		min = arr[0];
+		max = arr[n-1];
 		
-		if(dice1 == dice2 && dice2 == dice3) {
-			price = (10000+(dice2*1000));
-		} else if(dice1==dice2 || dice1 == dice3 || dice2 == dice3) {
-			if(dice1==dice2) {
-				price = 1000 + (dice1 * 100);
-			}
-			if(dice1==dice3) {
-				price = 1000 + (dice1 * 100);
-			}
-			if(dice3==dice2) {
-				price = 1000 + (dice3 * 100);
-			}
-		} 
-
-		bw.write(Integer.toString(price));
+		bw.write(min + " " + max);
 		bw.flush();
 		bw.close();
 		br.close();
