e616995c87eb7cbbcc8db615e4a8e9cf1bfc2545
nuguseyo024 <nuguseyo024@naver.com>
2022-09-23 15:21:15 +0900

    22-09-23 백준 1546번 :: 평균 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,20 +11,29 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
-		final int N = 42;
-		int[] arr = new int[10];
+//		예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+//		세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
 		
-		HashSet<Integer> set = new HashSet<Integer>();
+		st = new StringTokenizer(br.readLine()," ");
+		float[] arr = new float[n];
+		float max = 0;
+		float[] newArr = new float[n];
+		float sum = 0;
 		
-		for(int i = 0; i < arr.length; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			set.add(arr[i] % N);
-			
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			newArr[i] = (arr[i]/max)*100;
+			sum += newArr[i];
 		}
 		
-		bw.write(Integer.toString(set.size()));
-		
+		System.out.println(sum/n);
 		bw.flush();
 		bw.close();
 		br.close();
