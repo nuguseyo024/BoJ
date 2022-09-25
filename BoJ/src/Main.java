c8e219b0ffb3e36bb81fc6ed82c45420cbfcf7bd
nuguseyo024 <nuguseyo024@naver.com>
2022-09-25 21:53:17 +0900

    22-09-25 백준 8958번 :: OX퀴즈- 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -5,35 +5,42 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
-//		예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
-//		세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		
-		st = new StringTokenizer(br.readLine()," ");
-		float[] arr = new float[n];
-		float max = 0;
-		float[] newArr = new float[n];
-		float sum = 0;
+		int score = 0;
+		int sum[] = new int[n];
+		
+		char x;
 		
 		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			if(arr[i] > max) {
-				max = arr[i];
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			for(int j = 0; j < str.length(); j++) {			
+				x = str.charAt(j);
+				String xstr = String.valueOf(x);
+				
+				if(xstr.equals("O")) {
+					score++;
+					sum[i] += score;
+				}else if(xstr.equals("X")) {
+					score = 0;
+				}
+				
 			}
+			score = 0;
 		}
 		for(int i = 0; i < n; i++) {
-			newArr[i] = (arr[i]/max)*100;
-			sum += newArr[i];
+			bw.write(Integer.toString(sum[i]));
+			bw.write("\n");
 		}
 		
-		System.out.println(sum/n);
+		
 		bw.flush();
 		bw.close();
 		br.close();
