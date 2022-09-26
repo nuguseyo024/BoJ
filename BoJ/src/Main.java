edae0ce05379d08c86238428244837ef1cb8653c
nuguseyo024 <nuguseyo024@naver.com>
2022-09-26 10:48:49 +0900

    22-09-26 백준 4344번 :: 평균은 넘겠지 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -7,42 +7,39 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		
-		int score = 0;
-		int sum[] = new int[n];
-		
-		char x;
-		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			for(int j = 0; j < str.length(); j++) {			
-				x = str.charAt(j);
-				String xstr = String.valueOf(x);
-				
-				if(xstr.equals("O")) {
-					score++;
-					sum[i] += score;
-				}else if(xstr.equals("X")) {
-					score = 0;
-				}
-				
+		int testcase = Integer.parseInt(st.nextToken());
+
+		for(int i = 0; i < testcase; i ++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int n = Integer.parseInt(st.nextToken());
+			
+			int arr[] = new int[n];
+			
+			double sum = 0; 
+			double mean = 0;
+			
+			for(int j = 0; j < n; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+				sum += arr[j];
 			}
-			score = 0;
+			//System.out.println("sum:"+sum);
+			mean = (sum/n);
+			//System.out.println("mean:"+mean);
+			double count = 0;
+			for(int j = 0; j < n; j++) {
+				if(arr[j] > mean) {
+					
+					count++;
+				}	
+			}
+			//System.out.println("count:"+count);
+			System.out.printf("%.3f%%",(count/n)*100);
+			System.out.println();
 		}
-		for(int i = 0; i < n; i++) {
-			bw.write(Integer.toString(sum[i]));
-			bw.write("\n");
-		}
-		
-		
-		bw.flush();
-		bw.close();
+
 		br.close();
 		
 	}
