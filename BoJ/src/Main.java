e69446d9fecb3082615af320de73ea5d5f6a4964
nuguseyo024 <nuguseyo024@naver.com>
2022-09-27 11:49:06 +0900

    22-09-27 백준 4344번 :: 평균은 넘겠지 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -1,47 +1,11 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int testcase = Integer.parseInt(st.nextToken());
-
-		for(int i = 0; i < testcase; i ++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int n = Integer.parseInt(st.nextToken());
-			
-			int arr[] = new int[n];
-			
-			double sum = 0; 
-			double mean = 0;
-			
-			for(int j = 0; j < n; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-				sum += arr[j];
-			}
-			//System.out.println("sum:"+sum);
-			mean = (sum/n);
-			//System.out.println("mean:"+mean);
-			double count = 0;
-			for(int j = 0; j < n; j++) {
-				if(arr[j] > mean) {
-					
-					count++;
-				}	
-			}
-			//System.out.println("count:"+count);
-			System.out.printf("%.3f%%",(count/n)*100);
-			System.out.println();
+class Test{
+	
+	long sum(int[] a) {
+		long sum = 0;
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
 		}
-
-		br.close();
-		
+		return sum;
 	}
-
+	
 }
