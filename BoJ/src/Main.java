89e8bf142dd60ab3d3163077a49a5a6df469ef4c
nuguseyo024 <nuguseyo024@naver.com>
2022-09-27 12:44:58 +0900

    백준 11654번 :: 아스키 코드 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -1,11 +1,23 @@
-class Test{
-	
-	long sum(int[] a) {
-		long sum = 0;
-		for(int i = 0; i < a.length; i++) {
-			sum += a[i];
-		}
-		return sum;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		char ch = st.nextToken().charAt(0);
+		int num = (int)ch;
+		
+		bw.write(Integer.toString(num));
+		bw.flush();
+		bw.close();
+		br.close();
+		
 	}
-	
 }
+
+
