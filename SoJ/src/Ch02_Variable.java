8dce934cc5a34b6cc098505dbcac16718a694ec5
nuguseyo024 <nuguseyo024@naver.com>
2022-09-28 18:15:02 +0900

    22-09-28 [자바의 정석] 챕터2 - 변수 연습문제

---
+++ SoJ/src/Ch02_Variable.java
@@ -0,0 +1,21 @@
+
+public class Ch02_Variable {
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		int tmp = 0;
+		
+		tmp = x;
+		x = y;
+		y = z;
+		z = tmp;
+		
+		
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		System.out.println("z=" + z);
+	}
+}
