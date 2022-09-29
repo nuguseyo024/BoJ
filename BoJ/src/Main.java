2a3eed2bd61dbc2146fee6a0c3b2dccb424594e2
nuguseyo024 <nuguseyo024@naver.com>
2022-09-29 16:56:41 +0900

    22-09-29 백준 5622번 :: 다이얼 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -8,27 +8,38 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-//		String a = new StringBuilder(st.nextToken()).reverse().toString();
-//		String b = new StringBuilder(st.nextToken()).reverse().toString();
-//		System.out.println(a+" "+b);
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		String str = st.nextToken();
+		char[] ch = new char[str.length()];
+		int[] num = new int[str.length()];
+		int sum = 0;
 		
-		int a1 = a / 100;
-		int a2 = (a - (a1*100)) / 10;
-		int a3 = (a - (a1*100) - (a2*10));	
-		int newA = (a3 * 100) + (a2 * 10) + a1;
+		for(int i = 0; i< str.length(); i++) {
+			ch[i] = str.charAt(i);
+			if(ch[i]=='A'||ch[i]=='B'||ch[i]=='C') {
+				num[i] = 2;
+			}else if(ch[i]=='D'||ch[i]=='E'||ch[i]=='F') {
+				num[i] = 3;
+			}else if(ch[i]=='G'||ch[i]=='H'||ch[i]=='I') {
+				num[i] = 4;
+			}else if(ch[i]=='J'||ch[i]=='K'||ch[i]=='L') {
+				num[i] = 5;
+			}else if(ch[i]=='M'||ch[i]=='N'||ch[i]=='O') {
+				num[i] = 6;
+			}else if(ch[i]=='P'||ch[i]=='Q'||ch[i]=='R'||ch[i]=='S') {
+				num[i] = 7;
+			}else if(ch[i]=='T'||ch[i]=='U'||ch[i]=='V') {
+				num[i] = 8;
+			}else if(ch[i]=='W'||ch[i]=='X'||ch[i]=='Y'||ch[i]=='Z') {
+				num[i] = 9;
+			}else {
+				num[i] = 1;
+			}
+			sum += (num[i]+1);
+		}
 		
-		int b1 = b / 100;
-		int b2 = (b - (b1*100)) / 10;
-		int b3 = (b - (b1*100) - (b2*10));
-		
-		int newB = (b3 * 100) + (b2 * 10) + b1;
-		
-		int big = (newA > newB)? newA: newB;
-		bw.write(Integer.toString(big));
+		bw.write(Integer.toString(sum));
 		
 		
 		bw.flush();
@@ -37,5 +48,12 @@ public class Main {
 		
 	}
 }
+
+//전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 
+//숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+//숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+//상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 
+//예를 들어, UNUCIC는 868242와 같다.
+//할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
 
 
