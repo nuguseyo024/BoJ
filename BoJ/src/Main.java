a7173c3e51eff1f7364111d0bdee72c92d87aea4
nuguseyo024 <nuguseyo024@naver.com>
2022-10-03 14:40:34 +0900

    백준 1008번 :: A/B - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
+	
 	public static void main(String[] args) throws Exception {
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int res = Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken());
