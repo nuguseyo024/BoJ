284ea055c87ca922f6fd48dbd71c9da8e6eb40b3
nuguseyo024 <nuguseyo024@naver.com>
2022-10-03 14:39:20 +0900

    백준 1000번 :: A+B - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -11,9 +11,9 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int res = Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken());
+		double res = (double)Integer.parseInt(st.nextToken()) / Integer.parseInt(st.nextToken());
 		
-		bw.write(Integer.toString(res));
+		bw.write(Double.toString(res));
 		bw.flush();
 		bw.close();
 		br.close();
