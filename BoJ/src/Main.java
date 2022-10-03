32c926820a44a20c2dbf882a90ea8106d1f6850d
nuguseyo024 <nuguseyo024@naver.com>
2022-10-03 15:16:23 +0900

    백준 15740 :: A+B - 9 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -9,18 +9,13 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	
 	public static void main(String[] args) throws Exception {
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int res = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
-		
-		
-		bw.write(Integer.toString(res));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");		
+		long res = Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken());
+
+		bw.write(Long.toString(res));
 		bw.flush();
 		bw.close();
 		br.close();
 	}
 }
-
-
