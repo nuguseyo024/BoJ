b25c229779827441527e90517e49a4b3c0a6304b
nuguseyo024 <nuguseyo024@naver.com>
2022-10-03 14:59:21 +0900

    백준 10998번 :: AxB - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -11,13 +11,12 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws Exception {
-		int n = Integer.parseInt(br.readLine());
-		for(int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(),",");
-			int res = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-			bw.write(Integer.toString(res)+"\n");
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
+		int res = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+		
+		
+		bw.write(Integer.toString(res));
 		bw.flush();
 		bw.close();
 		br.close();
