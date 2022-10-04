9fcea53da5900855c516b8968a00fed3bba79fa4
nuguseyo024 <nuguseyo024@naver.com>
2022-10-04 21:42:19 +0900

    백준 2577번 :: 숫자의 개수 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	
@@ -10,12 +9,42 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
 	public static void main(String[] args) throws Exception {
-		StringTokenizer st = new StringTokenizer(br.readLine());		
-		int n = Integer.parseInt(st.nextToken());
+		String a = br.readLine();
+		String b = br.readLine();
+		String c = br.readLine();
+		int res = Integer.parseInt(a) * Integer.parseInt(b) * Integer.parseInt(c);
+        String str = Integer.toString(res);
+        char[] ch = new char[str.length()];
+        int[] num = new int[10];
         
-		for(int i = n; i >= 1; i--) {
-			bw.write(Integer.toString(i));
-			bw.write("\n");
+		for(int i = 0; i < str.length(); i++) {
+			ch[i] = str.charAt(i);
+			if(ch[i] == 48) {
+				num[0]++;
+			}else if(ch[i] == 49) {
+				num[1]++;
+			}else if(ch[i] == 50) {
+				num[2]++;
+			}else if(ch[i] == 51) {
+				num[3]++;
+			}else if(ch[i] == 52) {
+				num[4]++;
+			}else if(ch[i] == 53) {
+				num[5]++;
+			}else if(ch[i] == 54) {
+				num[6]++;
+			}else if(ch[i] == 55) {
+				num[7]++;
+			}else if(ch[i] == 56) {
+				num[8]++;
+			}else if(ch[i] == 57) {
+				num[9]++;
+			}
+			
+		}
+		
+		for(int i = 0; i < num.length; i++) {
+			bw.write(num[i]+"\n");
 		}
 		
 		bw.flush();
