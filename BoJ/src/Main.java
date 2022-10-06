4b6d1d5b1b6a657faa3ba58f556c2849fc500603
yehyeong <yehyeong@wsmail.co.kr>
2022-10-06 14:31:24 +0900

    22-10-06 백준 10817번 :: 세 수 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -2,20 +2,43 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
+
 	public static void main(String[] args) throws Exception {
-		int n = Integer.parseInt(br.readLine());
-		int res = 1;
-		
-		for(int i = n; i > 1; i--) {
-			res *= i;
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int res = 0;
+
+		if(a >= b) {
+			if(a >= c) {
+				if(b >= c) {
+					res = b;
+				}else if( c >= b) {
+					res = c;
+				}
+			}else if( c >= a) {
+				res = a;
+			}
+		}else if(b >= a) {
+			if(b >= c) {
+				if(c >= a) {
+					res = c;
+				}else if(a >= c) {
+					res = a;
+				}
+			}else if(c >= b) {
+				res = b;
+			}
 		}
-		
+
+ 
 		bw.write(Integer.toString(res));
 		bw.flush();
 		bw.close();
