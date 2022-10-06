e8ef6ade55579002586a5fac0196e066e08cbe73
nuguseyo024 <nuguseyo024@naver.com>
2022-10-06 17:34:57 +0900

    백준 10039번 :: 평균 점수 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
 
@@ -10,28 +9,18 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws Exception {
-		int[] burgers = new int[3];
-		for(int i = 0; i < 3; i++) {
-			burgers[i] = Integer.parseInt(br.readLine());
+		int[] score = {Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine())};
+
+		int sum = 0;
+
+		for(int i = 0; i < score.length; i++) {
+			if(score[i]<40) {
+				score[i] = 40;
+			}
+			sum += score[i];
 		}
-
-		int[] drinks = new int[2];
-		for(int i = 0; i < 2; i++) {
-			drinks[i] = Integer.parseInt(br.readLine());
-		}
-
-		int[] sets = new int[6];
-
-
-		sets[0] = burgers[0]+drinks[0] -50 ;
-		sets[1] = burgers[0]+drinks[1] -50 ;
-		sets[2] = burgers[1]+drinks[0] -50 ;
-		sets[3] = burgers[1]+drinks[1] -50 ;
-		sets[4] = burgers[2]+drinks[0] -50 ;
-		sets[5] = burgers[2]+drinks[1] -50 ;
-
-		Arrays.sort(sets);
-		bw.write(Integer.toString(sets[0]));
+		int mean = sum/5;
+		bw.write(Integer.toString(mean));
 		bw.flush();
 		bw.close();
 		br.close();
