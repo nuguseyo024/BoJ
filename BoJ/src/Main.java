b6b650ce628bb150ad3ac45a06f380accbc0f339
nuguseyo024 <nuguseyo024@naver.com>
2022-10-06 17:42:02 +0900

    백준 2440번 :: 별 찍기 - 3 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -9,18 +9,13 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws Exception {
-		int[] score = {Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine()),Integer.parseInt(br.readLine())};
-
-		int sum = 0;
-
-		for(int i = 0; i < score.length; i++) {
-			if(score[i]<40) {
-				score[i] = 40;
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i < n; i++) {
+			for(int j = n; j > i; j--) {
+				bw.write("*");
 			}
-			sum += score[i];
+			bw.write("\n");
 		}
-		int mean = sum/5;
-		bw.write(Integer.toString(mean));
 		bw.flush();
 		bw.close();
 		br.close();
