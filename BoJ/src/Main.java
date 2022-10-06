b27317e6067ee9268dd3357a96f11ad6cf94c24c
nuguseyo024 <nuguseyo024@naver.com>
2022-10-06 17:46:57 +0900

    백준 2441번 :: 별 찍기 - 4 - 자바 Java

--- BoJ/src/Main.java
+++ BoJ/src/Main.java
@@ -11,6 +11,9 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		int n = Integer.parseInt(br.readLine());
 		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i; j++) {
+				bw.write(" ");
+			}
 			for(int j = n; j > i; j--) {
 				bw.write("*");
 			}
